171d4e48ef210f923f7080417322855cef6cb9c4
Suman-Limbu <sumanscoopie123@gmail.com>
2025-08-22 08:31:47 +0545

    formatted

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,39 +1,35 @@
 import mongoose from "mongoose";
 
-const  productSchema =  new mongoose.Schema({
-name:{type:String,
-    required: [true, "product name is required"]
-},
-brand: String,
-category: {type:String,
-    required: [true, "product category is required"]
-},
-price:{type:Number,
-    required: [true, "product price is required"],
-    min:[1,"product price must be positive number"]
-},
-createdAt: {
-    type:Date,
-    default: Date.now(),
-    immutable:true,
-},
-stock : {
-    type:Number,
-    default:1,
-max: [10000,"stock items must not exceed 1000 "]
-},
-size:{
-    type:String,
-    enum:["L","M","XL","XXL"]
-},
-imageUrls:{
-    type:[
-    String
-    ]
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: [true, "product name is required"] },
+    brand: String,
+    category: {
+      type: String,
+      required: [true, "product category is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "product price is required"],
+      min: [1, "product price must be positive number"],
+    },
 
-}
+    stock: {
+      type: Number,
+      default: 1,
+      max: [10000, "stock items must not exceed 10000"],
+    },
+    size: {
+      type: String,
+      enum: ["L", "M", "XL", "XXL"],
+    },
+    imageUrls: {
+      type: [String],
+    },
+  },
 
-});
+  { timestamps: true }
+);
 
 const model = mongoose.model("Product", productSchema);
 
